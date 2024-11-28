58ed4b5092ae682ecbb8363fab8375eaae5f821a
Its6ueq <totenhaibang@gmail.com>
2024-11-28 08:58:15 +0700

    Update img proce

--- back-end/numRecog.py
+++ back-end/numRecog.py
@@ -1,6 +1,7 @@
 
 import numpy as np
-from PIL import Image, ImageChops
+from PIL import Image, ImageChops, ImageOps
+import time
 
 imgHeight = 50
 imgWidth = 50
@@ -29,35 +30,74 @@ def trim_borders(image):
 
 def save_image(image, name):
     image = trim_borders(image)
+    image = make_square(image)
     image = replace_transparent_background(image)
-#    image = image.resize((imgHeight, imgWidth))
+    image = image.convert('L')
+    image = ImageOps.invert(image)
+    print('converted')
+    image = image.resize((8, 8))
     image.save(name)
     
-def dfs(arr, tempArr, i, j, width, height):
-    global count
-    if (i < 0 or j < 0 or i >= height or j >= width or arr[i, j, 3] == 0):
-        return 
-    tempArr[i, j, 3] = arr[i, j, 3]
-    arr[i, j, 3] = 0
-    dfs(arr, tempArr, i + 1, j, width, height)
-    dfs(arr, tempArr, i - 1, j, width, height)
-    dfs(arr, tempArr, i, j + 1, width, height)
-    dfs(arr, tempArr, i, j - 1, width, height)
+def make_square(image, min_size=28, fill_color=(0, 0, 0, 0)):
+    x, y = image.size
+    size = max(min_size, x, y)
+    newImage = Image.new('RGBA', (size, size), fill_color)
+
+    newImage.paste(image, (int((size - x) / 2), int((size - y) / 2)))
+    # newImage = ImageOps.expand(image, border=int(size/6), fill='#fff') 
+    return newImage
     
-def find_number(img):
+# def dfs1(arr, tempArr, i, j, width, height):
+#     global count
+#     if (i < 0 or j < 0 or i >= height or j >= width or arr[i, j, 3] == 0):
+#         return 
+#     tempArr[i, j, 3] = arr[i, j, 3]
+#     arr[i, j, 3] = 0
+#     dfs(arr, tempArr, i + 1, j, width, height)
+#     dfs(arr, tempArr, i - 1, j, width, height)
+#     dfs(arr, tempArr, i, j + 1, width, height)
+#     dfs(arr, tempArr, i, j - 1, width, height)
+    
+# def find_number(img):
+#     global count
+#     width = img.width
+#     height = img.height
+#     arr = np.asarray(img).copy()
+#     for i in range(height):
+#         for j in range(width):
+#             if (arr[i, j, 3] != 0):
+#                 count += 1
+#                 tempArr = np.zeros((height,width, 4), dtype = np.uint8)
+#                 dfs(arr, tempArr, i, j, width, height)
+#                 data = Image.fromarray(tempArr, mode = "RGBA")
+#                 save_image(data, str(count) + ".png")
+                
+def dfs_stack(img):
     global count
     width = img.width
     height = img.height
+    stack = [] 
     arr = np.asarray(img).copy()
     for i in range(height):
         for j in range(width):
             if (arr[i, j, 3] != 0):
+                stack.append((i, j))
                 count += 1
                 tempArr = np.zeros((height,width, 4), dtype = np.uint8)
-                dfs(arr, tempArr, i, j, width, height)
+                while stack:
+                    x, y = stack.pop() 
+                    
+                    # time.sleep(5)
+                    d = [-1, 0, 1, 0, -1]
+                    for k in range (0, 4):
+                        newX = x + d[k]
+                        newY = y + d[k + 1]
+                        if (newX >= 0 and newY >= 0 and newX < height and newY < width and arr[newX, newY, 3] != 0):
+                            stack.append((newX, newY))
+                            tempArr[newX, newY, 3] = arr[newX, newY, 3]
+                            arr[newX, newY, 3] = 0
                 data = Image.fromarray(tempArr, mode = "RGBA")
                 save_image(data, str(count) + ".png")
-    
 
 def solveImage():
     global count
@@ -66,9 +106,11 @@ def solveImage():
         img = Image.open("received_image.png") 
     except IOError:
         pass    
-    img1 = img.resize((100, 100))
-    img1.save("resized.png")
-    find_number(img1)
+
+    dfs_stack(img)
     print(count)
     return str(count)
+
+def pad_image(image):
+    return ImageOps.expand(image, border=30, fill='#fff')
 
